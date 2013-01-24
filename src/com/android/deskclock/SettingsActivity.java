68ad6b4d70797eff46e2670a2046b2598d53d83d
LorD ClockaN <davor@losinj.com>
2013-01-24 14:06:17 +0100

    Added flip and shake to snooze - by Artem Chep<artemchep@gmail.com>
    both the actions flipping the phone, and shaking the phone can do one of the following operations
    1. do nothing
    2. snooze the alarm
    3. dismiss the alarm
    
    has been taken from Artem AchepClock implementation for ICS deskclock
    source at http://github.com/Achep/android_packages_apps_DeskClock
    
    PatchSet2 : Eliminated White Spaces
    PatchSet3 : Fixed a line issue
    PatchSet4 : Updated Correct Author
    PatchSet5 : Really updated Author
    PatchSet6 : Fixed a few lines, in-line comments
    PatchSet7 : Cleanup; addressed Danny's comments
    PatchSet8 : More cleanup
    PatchSet10: Clean up string resources
    
    Change-Id: Ifea8185690b07923b0c7cb3ab9b33ad5da7306e8
    
    Conflicts:
    	src/com/android/deskclock/SettingsActivity.java

--- src/com/android/deskclock/SettingsActivity.java
+++ src/com/android/deskclock/SettingsActivity.java
@@ -52,6 +52,10 @@ public class SettingsActivity extends PreferenceActivity
             "show_status_bar_icon";
     static final String KEY_ALARM_SNOOZE =
             "snooze_duration";
+    static final String KEY_FLIP_ACTION =
+            "flip_action";
+    static final String KEY_SHAKE_ACTION =
+            "shake_action";
     static final String KEY_VOLUME_BEHAVIOR =
             "volume_button_setting";
     static final String KEY_AUTO_SILENCE =
@@ -187,6 +191,14 @@ public class SettingsActivity extends PreferenceActivity
             update.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
             update.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, widgetIds);
             getApplicationContext().sendBroadcast(update);
+        } else if (KEY_FLIP_ACTION.equals(pref.getKey())) {
+            final ListPreference listPref = (ListPreference) pref;
+            String action = (String) newValue;
+            updateActionSummary(listPref, action, R.string.flip_action_summary);
+        } else if (KEY_SHAKE_ACTION.equals(pref.getKey())) {
+            final ListPreference listPref = (ListPreference) pref;
+            String action = (String) newValue;
+            updateActionSummary(listPref, action, R.string.shake_action_summary);
         } else if (KEY_SHOW_STATUS_BAR_ICON.equals(pref.getKey())) {
             // Check if any alarms are active. If yes and
             // we allow showing the alarm icon, the icon will be shown.
@@ -203,6 +215,12 @@ public class SettingsActivity extends PreferenceActivity
         } else {
             listPref.setSummary(getString(R.string.auto_silence_summary, i));
         }
+    }
+
+    private void updateActionSummary(ListPreference listPref, String action, int summaryResId) {
+        int i = Integer.parseInt(action);
+        listPref.setSummary(getString(summaryResId,
+            getResources().getStringArray(R.array.action_summary_entries)[i]));
     }
 
     private void refresh() {
@@ -225,6 +243,14 @@ public class SettingsActivity extends PreferenceActivity
 
         listPref = (ListPreference) findPreference(KEY_VOLUME_BUTTONS);
         listPref.setSummary(listPref.getEntry());
+        listPref.setOnPreferenceChangeListener(this);
+
+        listPref = (ListPreference) findPreference(KEY_FLIP_ACTION);
+        updateActionSummary(listPref, listPref.getValue(), R.string.flip_action_summary);
+        listPref.setOnPreferenceChangeListener(this);
+
+        listPref = (ListPreference) findPreference(KEY_SHAKE_ACTION);
+        updateActionSummary(listPref, listPref.getValue(), R.string.shake_action_summary);
         listPref.setOnPreferenceChangeListener(this);
 
         CheckBoxPreference hideStatusbarIcon = (CheckBoxPreference) findPreference(KEY_SHOW_STATUS_BAR_ICON);
